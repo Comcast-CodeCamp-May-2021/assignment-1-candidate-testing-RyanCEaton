c862990cd476a2d81c9fa2a29378282ca28f6cbb
RyanCEaton <>
2021-05-12 22:12:32 +0000

    Final format update

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,12 +4,13 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ", 
+let questions = [
+  "Who was the first American woman in space? ", 
   "True or false: 5 kilometer == 5000 meters? ", 
   "(5 + 3)/2 * 10 = ? ", 
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
@@ -17,25 +18,21 @@ let questions = ["Who was the first American woman in space? ",
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
-
-
-function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+function askForName() {
   candidateName = input.question("What is your name?: ");
 }
 
-function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-for (let i = 0; i < 5; i++) {
-candidateAnswer = input.question(`${i + 1}) ${questions[i]}`);
-candidateAnswers.push(candidateAnswer);
+function askQuestion() {
+  for (let i = 0; i < 5; i++) {
+  candidateAnswer = input.question(`${i + 1}) ${questions[i]}`);
+  candidateAnswers.push(candidateAnswer);
   }
 }
 
-
-function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+function gradeQuiz(candidateAnswers) {
   let numberCorrect = 0;
   console.clear();
   console.log(`Candidate Name: ${candidateName}`);
@@ -51,18 +48,14 @@ function gradeQuiz(candidateAnswers) {
   } // the fact that this curly brace is here and not in the right place really bothers me, but it isn't a big deal at all because I really like how you formatted the loop
   
   //these could be on the same line
-  let grade = 0;
-
-  grade = numberCorrect / 5 * 100;
-  
-    console.log(`>>> Overall Grade: ${grade}% (${numberCorrect} of ${questions.length} responses correct) <<<`);
+    let grade = numberCorrect / 5 * 100;
+      console.log(`>>> Overall Grade: ${grade}% (${numberCorrect} of ${questions.length} responses correct) <<<`);
     if (grade >= '80') {
     console.log(">>> Status: PASSED <<<");
     }
       else {
       console.log(">>> Status: FAILED <<<");
       }
-
   return grade;
 }
 
