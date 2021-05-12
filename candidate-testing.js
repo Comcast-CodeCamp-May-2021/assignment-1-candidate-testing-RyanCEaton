db09c0bb0d02b3bac1944873e07f01eca9b73e81
RyanCEaton <>
2021-05-12 20:51:36 +0000

    Attempt #5

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,11 +8,10 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5 kilometers? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 let numberCorrect = 0;
-let grade = 0;
 
 
 function askForName() {
@@ -48,7 +47,7 @@ function gradeQuiz(candidateAnswers) {
         }
   
   
-  
+  let grade;
   grade = numberCorrect / questions.length * 100;
   
     console.log(`>>> Overall Grade: ${grade}% (${numberCorrect} of ${questions.length} responses correct) <<<`);
